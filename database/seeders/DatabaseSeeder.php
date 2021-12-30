941a3e307e2c972d875c3fceda3feab82f251c3c
SteveOwago <33925746+SteveOwago@users.noreply.github.com>
2021-12-30 15:39:48 +0300

    Chater seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             RoleUserTableSeeder::class,
             CrmStatusTableSeeder::class,
             TaskStatusTableSeeder::class,
+            ChatterTableSeeder::class,
 
         ]);
     }
